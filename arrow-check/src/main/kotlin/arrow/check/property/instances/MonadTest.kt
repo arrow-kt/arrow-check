b22ce37da85183970791be4f82e13ec133c82883
Jannis <overesch.jannis@gmail.com>
2020-01-29 10:53:01 +0100

    Try MonadTest instances without using extension
    Let's see if this helps..

--- arrow-check/src/main/kotlin/arrow/check/property/instances/MonadTest.kt
+++ arrow-check/src/main/kotlin/arrow/check/property/instances/MonadTest.kt
@@ -3,14 +3,16 @@ package arrow.check.property.instances
 import arrow.Kind
 import arrow.check.property.MonadTest
 import arrow.check.property.Test
-import arrow.extension
 import arrow.mtl.*
 import arrow.mtl.extensions.*
 import arrow.mtl.extensions.optiont.monadTrans.liftT
 import arrow.typeclasses.Monad
 import arrow.typeclasses.Monoid
 
-@extension
+fun <M> OptionT.Companion.monadTest(MT: MonadTest<M>): MonadTest<OptionTPartialOf<M>> = object : OptionTMonadTest<M> {
+    override fun MT(): MonadTest<M> = MT
+}
+
 interface OptionTMonadTest<M> : MonadTest<OptionTPartialOf<M>>, OptionTMonad<M> {
     override fun MF(): Monad<M> = MT()
     fun MT(): MonadTest<M>
@@ -19,7 +21,10 @@ interface OptionTMonadTest<M> : MonadTest<OptionTPartialOf<M>>, OptionTMonad<M> 
     }
 }
 
-@extension
+fun <M, L> EitherT.Companion.monadTest(MT: MonadTest<M>): MonadTest<EitherTPartialOf<M, L>> = object : EitherTMonadTest<M, L> {
+    override fun MT(): MonadTest<M> = MT
+}
+
 interface EitherTMonadTest<M, L> : MonadTest<EitherTPartialOf<M, L>>, EitherTMonad<M, L> {
     override fun MF(): Monad<M> = MT()
     fun MT(): MonadTest<M>
@@ -28,7 +33,10 @@ interface EitherTMonadTest<M, L> : MonadTest<EitherTPartialOf<M, L>>, EitherTMon
     }
 }
 
-@extension
+fun <M, D> Kleisli.Companion.monadTest(MT: MonadTest<M>): MonadTest<KleisliPartialOf<M, D>> = object : KleisliTMonadTest<M, D> {
+    override fun MT(): MonadTest<M> = MT
+}
+
 interface KleisliTMonadTest<M, D> : MonadTest<KleisliPartialOf<M, D>>, KleisliMonad<M, D> {
     override fun MF(): Monad<M> = MT()
     fun MT(): MonadTest<M>
@@ -37,7 +45,11 @@ interface KleisliTMonadTest<M, D> : MonadTest<KleisliPartialOf<M, D>>, KleisliMo
     }
 }
 
-@extension
+fun <M, W> WriterT.Companion.monadTest(MT: MonadTest<M>, MM: Monoid<W>): MonadTest<WriterTPartialOf<M, W>> = object : WriterTMonadTest<M, W> {
+    override fun MT(): MonadTest<M> = MT
+    override fun MM(): Monoid<W> = MM
+}
+
 interface WriterTMonadTest<M, W> : MonadTest<WriterTPartialOf<M, W>>, WriterTMonad<M, W> {
     override fun MM(): Monoid<W>
     override fun MF(): Monad<M> = MT()
@@ -48,7 +60,10 @@ interface WriterTMonadTest<M, W> : MonadTest<WriterTPartialOf<M, W>>, WriterTMon
     }
 }
 
-@extension
+fun <M, S> StateT.Companion.monadTest(MT: MonadTest<M>): MonadTest<StateTPartialOf<M, S>> = object : StateTMonadTest<M, S> {
+    override fun MT(): MonadTest<M> = MT
+}
+
 interface StateTMonadTest<M, S> : MonadTest<StateTPartialOf<M, S>>, StateTMonad<M, S> {
     override fun MF(): Monad<M> = MT()
     fun MT(): MonadTest<M>
